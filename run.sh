9b5079003c562d0e0c8753d203a8d0f084bb95ca
Gerrit Admin <gerrit.admin@salcom.com>
2021-06-28 14:20:27 +1200

    uprevved trac to version 1.4.3, part 1

--- run.sh
+++ run.sh
@@ -2,4 +2,11 @@
 
 [ -f /.setup_trac.sh ] && /bin/bash /.setup_trac.sh
 
+ROOT=/trac
+TRACS="development maintenance administration sandbox"
+
+# upgrade database and wiki
+for TRAC in $TRACS; do trac-admin $ROOT/$TRAC upgrade; done
+for TRAC in $TRACS; do trac-admin $ROOT/$TRAC wiki upgrade; done
+
 tracd -p 80 -e /trac
--- setup_trac.sh
+++ setup_trac.sh
@@ -6,36 +6,12 @@ setup_trac() {
     [ ! -d $1 ] && mkdir $1
     if [ ! -f $1/VERSION ]
     then
-        trac-admin $1 initenv "$1" sqlite:db/trac.db git /repo.git
+        trac-admin $1 initenv "$1" sqlite:db/trac.db
         setup_components $1
         setup_accountmanager $1
         setup_admin_user $1
         trac-admin $1 config set logging log_type stderr
         [ -f /var/www/trac_logo.png ] && cp -v /var/www/trac_logo.png $1/htdocs/your_project_logo.png
-    fi
-}
-
-setup_repo() {
-    if [ ! -d /repo.git ]
-    then 
-        git config --global user.name "Trac Admin"
-        git config --global user.email trac@localhost
-
-        mkdir /repo.git
-        pushd /repo.git
-            git init --bare
-        popd
-
-        pushd /tmp
-            git clone --no-hardlinks /repo.git repo
-            pushd repo
-                echo repository init >README
-                git add README
-                git commit README -m "initial commit"
-                git push origin master
-            popd
-            rm -rf repo
-        popd
     fi
 }
 
@@ -50,6 +26,5 @@ clean_house() {
 ROOT=/trac
 TRACS="development maintenance administration sandbox"
 
-setup_repo
 for TRAC in $TRACS; do setup_trac $ROOT/$TRAC; done
 clean_house
